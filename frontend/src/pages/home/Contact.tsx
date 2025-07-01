896434f1efb66b1c06e0db9f2a2758a52eeedd25
luk-ryan <ryankdluk@gmail.com>
2025-07-01 13:37:36 -0400

    added Home page content

--- frontend/src/pages/home/Contact.tsx
+++ frontend/src/pages/home/Contact.tsx
@@ -4,9 +4,9 @@ const Contact = () => {
   return (
     <div>
       <h1>Order Here</h1>
-      <p>Email</p>
-      <p>Phone</p>
-      <p>Instagram</p>
+      <p>Email: info@moon-treatz.com</p>
+      <p>Phone: (647)-268-5408</p>
+      <p>Instagram: @moontreatzcatering</p>
     </div>
   );
 };
--- frontend/src/pages/home/Menu.tsx
+++ frontend/src/pages/home/Menu.tsx
@@ -4,9 +4,40 @@ export const Menu = () => {
   return (
     <div>
       <h1>Menu</h1>
-      <div>Order Instructions</div>
-      <div>Order Options</div>
-      <div>Dietary Restrictions</div>
+      <div>
+        <p>
+          Choose 1 flavour for every 10 macarons, up to 3 flavours per order
+        </p>
+      </div>
+      <div>
+        <table>
+          <tr>
+            <td>10 Macarons</td>
+            <td>$20</td>
+          </tr>
+          <tr>
+            <td>20 Macarons</td>
+            <td>$40</td>
+          </tr>
+          <tr>
+            <td>30 Macarons</td>
+            <td>$60</td>
+          </tr>
+          <tr>
+            <td>40 Macarons</td>
+            <td>$70</td>
+          </tr>
+          <tr>
+            <td>60 Macarons</td>
+            <td>$90</td>
+          </tr>
+        </table>
+      </div>
+      <div>
+        <h2>Dietary Restrictions</h2>
+        <p>contains almonds</p>
+        <p>dairy free</p>
+      </div>
     </div>
   );
 };
