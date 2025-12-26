4435716e19447074b77863d006c25ce4914d124b
luk-ryan <ryankdluk@gmail.com>
2025-12-26 03:36:33 -0500

    updated catering menu prices

--- frontend/src/pages/home/Menu.tsx
+++ frontend/src/pages/home/Menu.tsx
@@ -2,18 +2,21 @@ import MenuItem from "./MenuItem";
 import type { MenuItemProps } from "../../types/types";
 
 const items: MenuItemProps[] = [
-  { quantity: 20, price: 40, flavours: 2 },
-  { quantity: 30, price: 55, flavours: 3 },
-  { quantity: 60, price: 100, flavours: 3 },
-  { quantity: 90, price: 135, flavours: 3 },
+  { quantity: 20, price: 35, flavours: 2 }, // ($1.75 per macaron)
+  { quantity: 30, price: 50, flavours: 3 }, // ($1.66 per macaron)
+  { quantity: 60, price: 95, flavours: 3 }, // ($1.58 per macaron)
+  { quantity: 90, price: 135, flavours: 3 }, // ($1.50 per macaron)
 ];
 
 export const Menu = () => {
   return (
     <div>
       <h1>Catering Menu</h1>
-      <p>Here are our options for catering, where you will get to choose up to 3 flavours per order!
-          Please order at least one week in advanced so that we have enough time to prepare large orders.</p>
+      <p>
+        Here are our options for catering, where you will get to choose up to 3
+        flavours per order! Please order at least one week in advanced so that
+        we have enough time to prepare large orders.
+      </p>
       <div className="narrow-wrapper">
         <div className="menu-list">
           {items.map((item) => (
@@ -35,20 +38,30 @@ export const Menu = () => {
 
         <div className="order-instructions">
           <h3>Order Instructions</h3>
-          <p><em>* Keep macaron box orders refrigerated.*</em></p>
+          <p>
+            <em>* Keep macaron box orders refrigerated.*</em>
+          </p>
           <p>
             If you would like to place an order, message us on Instagram or send
             us an email, and we will get back to you as soon as we can.
           </p>
-          <p>Both <strong>Pickup</strong> and <strong>Delivery</strong> options are available:</p>
+          <p>
+            Both <strong>Pickup</strong> and <strong>Delivery</strong> options
+            are available:
+          </p>
           <ul>
             <li>
-              <strong>Pickup</strong> will be at our location listed below (exact address will be
-              given out upon ordering).
+              <strong>Pickup</strong> will be at our location listed below
+              (exact address will be given out upon ordering).
             </li>
-            <li><strong>Delivery fee</strong> of $2 - $5</li>
+            <li>
+              <strong>Delivery fee</strong> of $2 - $5
+            </li>
           </ul>
-          <p>Available Payment Options: <strong>Cash</strong> and <strong>E-transfer</strong></p>
+          <p>
+            Available Payment Options: <strong>Cash</strong> and{" "}
+            <strong>E-transfer</strong>
+          </p>
         </div>
       </div>
     </div>
